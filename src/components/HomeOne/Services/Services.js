21dcd05927995e81abc7cde076e3f3891dc45026
AmmarAli2025 <102882653+AmmarAli2025@users.noreply.github.com>
2023-12-28 14:55:26 +0200

    Your commit message here

--- src/components/HomeOne/Services/Services.js
+++ src/components/HomeOne/Services/Services.js
@@ -1,7 +1,7 @@
 import React from "react";
-import image from "../../../assets/images/icons8-money-50.png";
-import image1 from "../../../assets/images/icons8-discount-50.png";
-import image2 from "../../../assets/images/icons8-support-50.png";
+import image from "../../../assets/images/icons8-money-50.webp";
+import image1 from "../../../assets/images/icons8-discount-50.webp";
+import image2 from "../../../assets/images/icons8-support-50.webp";
 import "./Services.scss";
 
 function Services() {
